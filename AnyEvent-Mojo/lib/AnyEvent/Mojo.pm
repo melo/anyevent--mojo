a48f80764cfe174d169712c607606cb6ebdd562c
Pedro Melo <melo@simplicidade.org>
2008-10-21 17:30:20 +0100

    Added logic to deal with a single request per connection
    So no keep_alive for now.
    
    Pretty good ab result:
    
     * run a server with: perl -MEV -MAnyEvent::Mojo -e 'AnyEvent::Mojo->new->run'
     * ab -n 1000 -c 1 http://127.0.0.1:3000/
    
    Results:
    
    Server Software:
    Server Hostname:        127.0.0.1
    Server Port:            3000
    
    Document Path:          /
    Document Length:        38 bytes
    
    Concurrency Level:      1
    Time taken for tests:   1.442904 seconds
    Complete requests:      1000
    Failed requests:        0
    Write errors:           0
    Total transferred:      159000 bytes
    HTML transferred:       38000 bytes
    Requests per second:    693.05 [#/sec] (mean)
    Time per request:       1.443 [ms] (mean)
    Time per request:       1.443 [ms] (mean, across all concurrent requests)
    Transfer rate:          107.42 [Kbytes/sec] received
    
    Connection Times (ms)
                  min  mean[+/-sd] median   max
    Connect:        0    0   0.0      0       0
    Processing:     1    1   0.1      1       2
    Waiting:        0    0   0.3      0       1
    Total:          1    1   0.1      1       2
    
    Percentage of the requests served within a certain time (ms)
      50%      1
      66%      1
      75%      1
      80%      1
      90%      1
      95%      1
      98%      1
      99%      1
     100%      2 (longest request)
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- AnyEvent-Mojo/lib/AnyEvent/Mojo.pm
+++ AnyEvent-Mojo/lib/AnyEvent/Mojo.pm
@@ -3,7 +3,7 @@ package AnyEvent::Mojo;
 use strict;
 use warnings;
 use base 'Mojo::Server';
-use Carp 'croak';
+use Carp qw( croak );
 use AnyEvent;
 use AnyEvent::Socket;
 
@@ -17,16 +17,40 @@ __PACKAGE__->attr('alive', chained => 1);
 sub listen {
   my $self = shift;
   
-  tcp_server undef, $self->port, sub {
-    my ($fh) = @_
-        or die "Connect failed: $!";
-
-    # ... deal with connection
-  }, sub {
-    my ($fh, $thishost, $thisport) = @_;
+  tcp_server(undef, $self->port,
+    # on connection
+    sub {
+      my ($fh) = @_;
+      
+      if (!$fh) {
+        print STDERR "Connect failed: $!\n";
+        return;
+      }
+      
+      my $tx = $self->build_tx_cb->($self);
+      $tx->state('read');
+      # Keep it simple, no keep-alive for now
+      $tx->res->headers->connection('close');
+      
+      # Create out magic handle
+      my $handle; $handle = AnyEvent::Handle->new(
+        fh       => $fh,
+        timeout  =>  15,
+        on_eof     => sub { undef $handle; undef $tx },
+        on_error   => sub { undef $handle; undef $tx },
+        on_timeout => sub { undef $handle; undef $tx },
+      );
+      
+      $handle->push_read(sub { $self->_read($tx, @_) });      
+    },
     
-    print "Server available at http://$thishost:$thisport/\n";
-  };
+    # Startup phase
+    sub {
+      my ($fh, $thishost, $thisport) = @_;
+      
+      print "Server available at http://$thishost:$thisport/\n";
+    }
+  );
 }
 
 sub run {
@@ -38,6 +62,99 @@ sub run {
   $self->alive($cv);
   
   $cv->recv;
+}
+
+
+##############
+# Read request
+
+sub _read {
+  my ($self, $tx, $handle) = @_;
+  my $req = $tx->request;
+  
+  return unless defined $handle->{rbuf};
+  
+  $req->parse(delete $handle->{rbuf});
+
+  my $done = $req->is_state(qw/done error/);
+  if ($done) {
+    $tx->state('write');
+    $self->handler_cb->($self, $tx);
+    
+    $self->_write($tx, $handle);
+  }
+  
+  return $done;
+};
+
+
+################
+# Write response
+
+sub _write {
+  my ($self, $tx, $handle) = @_;
+
+  $handle->on_drain(sub {
+    $self->_write_more($tx, $handle);
+  });
+}
+
+sub _write_more {
+  my ($self, $tx, $handle) = @_;
+  my $res = $tx->res;
+  my $state = $tx->state;
+  my $chunk;
+
+  # Advance Mojo state machine
+  if ($tx->is_state('write')) {
+    $tx->state('write_start_line');
+    $tx->{_to_write} = $tx->res->start_line_length;
+  }
+
+  # Response start line
+  if ($tx->is_state('write_start_line') && $tx->{_to_write} <= 0) {
+    $tx->state('write_headers');
+    $tx->{_offset} = 0;
+    $tx->{_to_write} = $tx->res->header_length;
+  }
+
+  # Response headers
+  if ($tx->is_state('write_headers') && $tx->{_to_write} <= 0) {
+    $tx->state('write_body');
+    $tx->{_offset} = 0;
+    $tx->{_to_write} = $tx->res->body_length;
+  }
+
+  # Response body
+  if ($tx->is_state('write_body') && $tx->{_to_write} <= 0) {
+    # FIXME: need AnyEvent::Mojo::Request to undo here
+    $handle->on_drain(undef);
+    close($handle->fh);
+    return;
+  }
+  
+  # Write the next chunk
+  my $offset = $tx->{_offset} || 0;
+  my $tw = $tx->{_to_write} || 0;
+  
+  # Body?
+  $chunk = $res->get_body_chunk($offset)
+    if $tx->is_state('write_body');
+  
+  # Headers?
+  $chunk = $res->get_header_chunk($offset)
+    if $tx->is_state('write_headers');
+  
+  # Start line?
+  $chunk = $res->get_start_line_chunk($offset)
+    if $tx->is_state('write_start_line');
+
+  # Advance internal counters, the data is ours responsability now  
+  my $written = length($chunk);
+  $tw = $tx->{_to_write}   -= $written;
+  $offset = $tx->{_offset} += $written;
+  
+  $handle->push_write($chunk);
 }
 
 
