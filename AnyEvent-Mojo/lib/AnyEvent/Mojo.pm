44a5a8ef2a2ad1f68c7e7b53af24b90d812e33f3
Pedro Melo <melo@simplicidade.org>
2009-10-04 09:14:03 +0100

    Adjust Copyright years
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- AnyEvent-Mojo/lib/AnyEvent/Mojo.pm
+++ AnyEvent-Mojo/lib/AnyEvent/Mojo.pm
@@ -152,7 +152,7 @@ Sebastian Riedel
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Pedro Melo.
+Copyright 2008-2009 Pedro Melo.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
--- AnyEvent-Mojo/lib/AnyEvent/Mojo/Server.pm
+++ AnyEvent-Mojo/lib/AnyEvent/Mojo/Server.pm
@@ -345,7 +345,7 @@ Pedro Melo, C<< <melo at cpan.org> >>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Pedro Melo.
+Copyright 2008-2009 Pedro Melo.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
